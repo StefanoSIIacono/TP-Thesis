7d1903fd5aa6feeff3fd5dd7bb9274e14417ea25
StefanoSIIacono <stefano.iacono@studenti.polito.it>
2025-02-20 10:56:29 +0100

    Final requirements + deleted unnecessary files

--- classes/asset.py
+++
@@ -1,3 +0,0 @@
-class Asset:
-    def __inti__ (self, name):
-        self.name
--- constants/parts.py
+++
@@ -1,8 +0,0 @@
-APP = 'a' # Application
-OS = 'o' # Operating System
-HW = 'h' # Hardware
-APP_OS = 'a|o' # Application or Operating System
-APP_HW = 'a|h' # Application or Hardware
-OS_HW = 'o|h', # Operating System or Hardware
-APP_OS_HW = 'a|o|h' # Application, Operating System, or Hardware
-         
